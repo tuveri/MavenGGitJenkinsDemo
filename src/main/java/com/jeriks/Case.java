64c57aa5f86936c38c6546786a3ffb3846802cb7
jan eriksson <janeriksson@jans-MacBook-Pro.local>
2021-03-24 00:08:18 +0100

    Small corrections

--- src/main/java/com/jeriks/Case.java
+++ src/main/java/com/jeriks/Case.java
@@ -2,11 +2,16 @@ package com.jeriks;
 
 public class Case {
 
-	private int caseid;
-	private String casetype;
+	private int caseid = 0;
+	private String casetype = "";
 
-	public void Case(int id, string type) {
+	public void Case(int id, String type) {
 		caseid = id;
 		casetype = type;
+	}
+
+	public String getCaseType(int id) {
+
+		return casetype;
 	}
 }
